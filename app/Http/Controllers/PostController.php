29edff98347c270637546e0d321b46fd4372fa31
asmit nepali <mail2asmitnepali@gmail.com>
2021-03-30 09:46:42 +0545

    [IMP] Laravel-Tutorial: Query Builder.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use DB;
 class PostController extends Controller
 {
     public function index($slug) {
-    	$posts = \DB::table('posts')->first();
+    	// Query Builder
+    	$posts = DB::table('posts')->where('slug',$slug)->first();
+    	if(! $posts) {
+    		abort(404);
+    	}
 	    return view('post',[
 	    	'post' => $posts,
 	    ]);
