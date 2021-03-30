ee98dd8bb4daa4d09836cfe2173e315933167c74
asmit nepali <mail2asmitnepali@gmail.com>
2021-03-30 16:42:37 +0545

    [IMP] Laravel-Tutorial: Little Introduction of Eloquent.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,15 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 use DB;
 class PostController extends Controller
 {
     public function index($slug) {
     	// Query Builder
-    	$posts = DB::table('posts')->where('slug',$slug)->first();
-    	if(! $posts) {
-    		abort(404);
-    	}
+    	$posts = Post::where('slug',$slug)->firstOrFail();
 	    return view('post',[
 	    	'post' => $posts,
 	    ]);
