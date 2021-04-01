3ee9899b3521a50645043435f4f9cd7d7d7d6dae
asmit nepali <mail2asmitnepali@gmail.com>
2021-04-01 17:52:08 +0545

    [IMP] Laravel-Tutorial: The Seven Restful Controller Actions.

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -7,8 +7,39 @@ use App\Models\Article;
 
 class ArticleController extends Controller
 {
+	// List all the resources.
+	public function index() {
+
+	}
+
+	//  Show single resource.
     public function show($articleId) {
     	$article = Article::find($articleId);
     	return view('articles.show', compact('article'));
+    }
+
+    // Show a view to create a new resource
+    public function create() {
+
+    }
+
+    // Persist the new resource.
+    public function store() {
+
+    }
+
+    // Show a view to edit an existing resource.
+    public function edit() {
+
+    }
+
+    // Persist the edited resource.
+    public function update() {
+
+    }
+
+    // Delete the resource.
+    public function destroy() {
+
     }
 }
